d06a9a49c08369a65e1c460dacf95055a06edbb0
Parkseho <sep775@naver.com>
2023-06-12 16:49:08 +0900

    error.java

--- src/gui/MemberViewer.java
+++ src/gui/MemberViewer.java
@@ -1,27 +1,49 @@
 package gui;
 
+import java.util.Vector;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-public class MemberViewer extends JFrame{
+import manager.MemberManager;
+import members.MemberInput;
 
-	public MemberViewer() {
+public class MemberViewer extends JPanel{
+	
+	WindowFrame frame;
+	
+	MemberManager membermanager;
+
+	public MemberViewer(WindowFrame frame, MemberManager membermanager) {
+		this.frame = frame;
+		this.membermanager = membermanager;
+		
+		System.out.println("***"+membermanager.size()+"***");
+		
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("ID");
 		model.addColumn("Name");
 		model.addColumn("Email");
 		model.addColumn("Contact Info.");
 		
+		for(int i = 0; i < membermanager.size(); i++) {
+			Vector row = new Vector();
+			MemberInput mi = membermanager.get(i);
+			row.add(mi.getId());
+			row.add(mi.getName());
+			row.add(mi.getEmail());
+			row.add(mi.getPhone());
+			model.addRow(row);
+		}
+		
 		JTable table = new JTable(model); 
 		JScrollPane sp = new JScrollPane(table);
 		
 		this.add(sp);
-		this.setSize(300, 300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
 	}
 
 }
