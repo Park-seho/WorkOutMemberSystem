e8626483922fa94b5b35a8ed9a0f82cbf342d56c
Parkseho <sep775@naver.com>
2023-06-12 18:29:44 +0900

    error.java

---
+++ src/manager/MemberManager.java
@@ -0,0 +1,159 @@
+package manager;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import members.BronzeMember;
+import members.DiamondMember;
+import members.GoldMember;
+import members.Member;
+import members.MemberInput;
+import members.MemberKind;
+
+public class MemberManager implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2938285357732514410L;
+
+	ArrayList<MemberInput> members = new ArrayList<MemberInput>();
+
+	transient Scanner input;
+
+	MemberManager(Scanner input){
+		this.input = input;
+	}
+
+	public void addMember() {
+		int kind = 0;
+		MemberInput memberInput;
+		while(kind < 1 || kind > 3) {
+			try {
+				System.out.println("1 for Diamond ");
+				System.out.println("2 for Gold ");
+				System.out.println("3 for Bronze ");
+				System.out.print("Select num for Member Kind between 1 and 3: ");
+				kind = input.nextInt();
+				if(kind == 1) {
+					memberInput = new DiamondMember(MemberKind.Diamond);
+					memberInput.getUserInput(input);
+					members.add(memberInput);
+					break;
+				}
+				else if(kind == 2) {
+					memberInput = new GoldMember(MemberKind.Gold);
+					memberInput.getUserInput(input);
+					members.add(memberInput);
+					break;
+				}
+				else if(kind == 3) {
+					memberInput = new BronzeMember(MemberKind.Bronze);
+					memberInput.getUserInput(input);
+					members.add(memberInput);
+					break;
+				}
+				else {
+					System.out.print("Select num for Member Kind between 1 and 3: ");
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 3!");
+				if(input.hasNext()) {
+					input.next();
+				}
+				kind = -1;
+			}
+		}
+	}
+
+	public void deleteMember() {
+
+		System.out.print("Member Id: ");
+		int memberId = input.nextInt();
+		int index = findIndex(memberId);
+		removefromMembers(index, memberId);
+	}
+
+	public int findIndex(int memberId) {
+		int index = -1;
+		for(int i = 0; i < members.size(); i++){
+			if(members.get(i).getId() == memberId) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+
+	public int removefromMembers(int index, int memberId) {
+		if(index >= 0) {
+			members.remove(index);
+			System.out.println("the member " + memberId + " is deleted");
+			return 1;
+		}
+		else {
+			System.out.println("the member has not been registered");
+			return -1;
+		}
+	}
+
+	public void editMember() {
+		System.out.print("Member Id: ");
+		int memberId = input.nextInt();
+		for(int i = 0; i <members.size(); i++){
+			MemberInput member = members.get(i);
+			if(member.getId() == memberId) {
+				int num = -1;
+				while(num != 5) {
+					showEditMenu();
+					num = input.nextInt();
+					switch(num) {
+					case 1:
+						member.setMemberID(input);
+						break;
+					case 2:
+						member.setMemberName(input);
+						break;
+					case 3:
+						member.setMemberEmail(input);
+						break;
+					case 4:
+						member.setMemberPhone(input);
+						break;
+					default:
+						continue;	
+					}
+				}
+				break;
+			}
+		}
+	}
+	public void viewMembers() {
+		System.out.println("# of registered members: " + members.size());
+		for(int i = 0; i <members.size(); i++){
+			members.get(i).printInfo();
+		}
+	}
+	
+	public int size() {
+		return members.size();
+	}
+	
+	
+	public MemberInput get(int index) {
+		return (Member) members.get(index);
+	}
+
+
+	public void showEditMenu() {
+		System.out.println("**Member Info Edit Menu**");
+		System.out.println("1. Edit ID ");
+		System.out.println("2. Edit Name ");
+		System.out.println("3. Edit Email ");
+		System.out.println("4. Edit Phone ");
+		System.out.println("5. Exit ");
+		System.out.println("Select one number between 1-5: ");
+	}
+}
---
+++ src/manager/MenuManager.java
@@ -0,0 +1,125 @@
+
+package manager;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import gui.WindowFrame;
+import log.EventLogger;
+
+public class MenuManager {
+	static EventLogger logger = new EventLogger("log.txt");
+
+	public static void main(String[] args) {
+
+
+		Scanner input = new Scanner(System.in);
+		MemberManager membermanager = getObject("membermanager.ser");
+		if(membermanager == null) {
+			membermanager = new MemberManager(input);
+		}
+		
+		WindowFrame frame = new WindowFrame(membermanager);
+		selectmenu(input, membermanager);
+		setObject(membermanager, "membermanager.ser");
+	}
+
+	public static void selectmenu(Scanner input, MemberManager membermanager) {
+		int num = -1;
+		while(num != 5) {
+			try {
+				showmenu();
+				num = input.nextInt();
+				switch(num) {
+				case 1:
+					membermanager.addMember();
+					logger.log("add a member");
+					break;
+				case 2:
+					membermanager.deleteMember();
+					logger.log("delete a member");
+					break;
+				case 3:
+					membermanager.editMember();
+					logger.log("edit a member");
+					break;
+				case 4:
+					membermanager.viewMembers();
+					logger.log("view members");
+					break;
+				default:
+					continue;
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 5!");
+				if(input.hasNext()) {
+					input.next();
+				}
+				num = -1;
+			}
+		}
+	}
+
+	public static void showmenu() {
+		System.out.println("***Member System Menu***");
+		System.out.println("1. Add Members ");
+		System.out.println("2. Delete Members ");
+		System.out.println("3. Edit Members ");
+		System.out.println("4. View Members ");
+		System.out.println("5. Exit ");
+		System.out.println("Select one number between 1-5: ");
+	}
+
+	public static MemberManager getObject(String filename) {
+		MemberManager membermanager = null;
+
+		try {
+			FileInputStream file = new FileInputStream(filename);
+			ObjectInputStream in = new ObjectInputStream(file);
+
+			membermanager = (MemberManager)in.readObject();
+
+			in.close();
+			file.close();
+
+		} catch (FileNotFoundException e) {
+			return membermanager;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return membermanager;
+
+	}
+
+	public static void setObject(MemberManager membermanager, String filename) {
+
+		try {
+			FileOutputStream file = new FileOutputStream(filename);
+			ObjectOutputStream out = new ObjectOutputStream(file);
+
+			out.writeObject(membermanager);
+
+			out.close();
+			file.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
